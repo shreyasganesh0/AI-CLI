2c6e3b5769edcf106b580e7c0ed5c043695b4050
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2025-06-27 08:20:24 -0400

    Create verbose flag for metadata display

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def prompt():
 
@@ -11,16 +12,33 @@ def prompt():
     client = genai.Client(api_key=api_key)
     model = "gemini-2.0-flash-001"
 
-    if len(sys.argv) != 2 :
+    if len(sys.argv) < 2 :
 
         print("Invalid arguments passed", sys.argv)
         sys.exit(1)
 
     prompt = sys.argv[1]
 
-    resp = client.models.generate_content(model = model, contents = prompt)
+    v_flag = False
+    if len(sys.argv) == 3: 
+        if sys.argv[2] == "--verbose":
 
-    print(f"{resp.text}\nPrompt tokens: {resp.usage_metadata.prompt_token_count}\nResponse tokens:{resp.usage_metadata.candidates_token_count}\n")
+            v_flag = True
+        else:
+            print("Invalid arguments passed", sys.argv)
+            sys.exit(1)
+
+    messages = [types.Content(role = "user", parts = [types.Part(text = prompt)])]
+
+    resp = client.models.generate_content(model = model, contents = messages)
+
+    print(f"{resp.text}\n")
+
+
+    if v_flag == True:
+        print(f"User prompt: {prompt}\n" 
+              f"Prompt tokens: {resp.usage_metadata.prompt_token_count}\n" 
+              f"Response tokens:{resp.usage_metadata.candidates_token_count}\n")
 
 
 if __name__ == "__main__":
