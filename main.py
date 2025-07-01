a05bca110c7f85ffa001c291376b52bb60708f9c
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2025-07-01 13:21:55 -0400

    Change package manager to uv and update LLM to understand functions

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+
 def prompt():
+
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -30,9 +41,38 @@ def prompt():
 
     messages = [types.Content(role = "user", parts = [types.Part(text = prompt)])]
 
-    resp = client.models.generate_content(model = model, contents = messages)
+    schema_get_files_info = types.FunctionDeclaration(
+        name="get_files_info",
+        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
+    )
 
-    print(f"{resp.text}\n")
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    ) # tell the LLM what functions it can use
+
+    resp = client.models.generate_content(model = model,
+                                          contents = messages,
+                                          config = types.GenerateContentConfig(
+                                            tools=[available_functions], 
+                                            system_instruction=system_prompt
+                                            )
+                                        )
+
+    #print(f"{resp.text}\n")
+
+    for func in resp.function_calls:
+        print(f"Calling function: {func.name}({func.args})")
 
 
     if v_flag == True:
---
+++ test_main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ai-cli!")
+
+
+if __name__ == "__main__":
+    main()
